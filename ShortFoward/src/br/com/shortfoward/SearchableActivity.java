75f65c058d9be44d969a6de8e7997103af06b86e
DanielFidalgo <daniel.fidalgo@gmail.com>
2013-07-23 01:35:28 -0300

    first commit

---
+++ ShortFoward/src/br/com/shortfoward/SearchableActivity.java
@@ -0,0 +1,23 @@
+package br.com.shortfoward;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.SearchView;
+
+public class SearchableActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.search_layout);
+	    InputMethodManager inputManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        SearchView search = (SearchView)findViewById(R.id.searchview);
+	    search.setIconifiedByDefault(false);
+	    search.setQueryHint("Sufixo");
+	    inputManager.showSoftInput(search,InputMethodManager.SHOW_IMPLICIT);
+	    
+	    
+	    
+	    
+	}
+}
---
+++ ShortFoward/src/br/com/shortfoward/widget/ShortFowardWidget.java
@@ -0,0 +1,41 @@
+package br.com.shortfoward.widget;
+
+
+
+import br.com.shortfoward.R;
+import br.com.shortfoward.SearchableActivity;
+import android.app.PendingIntent;
+import android.app.SearchManager;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class ShortFowardWidget extends AppWidgetProvider {
+	private static String CLICK_ACTION_SEARCH ="search";
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		super.onReceive(context, intent);
+		
+		if (intent.getAction().equals(CLICK_ACTION_SEARCH)) {
+			Intent intentShare = new Intent(context,SearchableActivity.class);
+	    	intentShare.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+	        context.startActivity(intentShare);
+		}
+		
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+		intent.setAction(CLICK_ACTION_SEARCH);
+		//Toast.makeText(context, R.string.rastreamento_iniciado, Toast.LENGTH_SHORT).show();
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+		views.setOnClickPendingIntent(R.id.btn, pendingIntent);
+		ComponentName cn = new ComponentName(context, ShortFowardWidget.class);
+		AppWidgetManager.getInstance(context).updateAppWidget(cn, views);
+		
+		
+	}
+}
