c990c578cc410dd0eac5da6994575d7fe3d332fe
DanielFidalgo <daniel.fidalgo@gmail.com>
2013-08-05 00:14:52 -0300

    melhoria

--- ShortFoward/src/br/com/shortfoward/SearchableActivity.java
+++ ShortFoward/src/br/com/shortfoward/SearchableActivity.java
@@ -11,7 +11,7 @@ public class SearchableActivity extends Activity {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.search_layout);
 	    InputMethodManager inputManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-        SearchView search = (SearchView)findViewById(R.id.searchview);
+        SearchView search = (SearchView)findViewById(R.id.sufixo);
 	    search.setIconifiedByDefault(false);
 	    search.setQueryHint("Sufixo");
 	    inputManager.showSoftInput(search,InputMethodManager.SHOW_IMPLICIT);
